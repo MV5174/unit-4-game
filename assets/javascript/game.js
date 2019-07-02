5275d476bedeb2c4e54992c6897be9b05434b82b
Michael Vega <ichigo509@yahoo.com>
2019-07-01 22:28:07 -0400

    worked on moving the cards and differentiating between the cards on attacking and choosing cards

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,116 @@
 $(document).ready(function(){
 
-//Global variables for each character (HP, AP, CAP)
+//Global variables
 var characterChosen = false;
+var opponentChosen = false;
 var chosenCharacter;
 var artorias = {
     HP: 125,
-    AP: 10,
-    CAP: 10,
+    AP: 12,
+    CAP: 5,
+    isAlive: true,
 }
+console.log(artorias);
+$("#artoriasHP").text(artorias.HP);
+$(".btn").toggle();
 //when the game starts choose a character
-$(".card").on("click", function chooseCharacter(event){
+$(".card").on("click", function chooseCharacter(){
     if(!characterChosen){
         characterChosen = true;
         console.log(characterChosen);
         chosenCharacter = this;
         console.log(chosenCharacter);
-        var target = $( event.target );
         $(this).appendTo("#playerRow");
-        
+        //the rest of the characters are unavailable to the player
         $("#characters").toggle();
         $("#characters").appendTo("#opponentSelectionRow");
         $("#characters").toggle();
+        $("button").filter($("#opponentSelectionRow button.choose")).toggle();
+    }
+    
+})
+//player must choose a character to fight from remaining pool
+//once opponent is chosen move to defender area
+//solaire functions
+$("#solaireButton").on("click", function chooseOpponent(){
+    if(!opponentChosen){
+    opponentChosen = true;
+    $("#solaire").appendTo("#defenderRow");
+    $("button").filter($("#defenderRow button.btn")).toggle();
+    $("button").filter($("#opponentSelectionRow button.choose")).toggle();
+}
+})
+
+$("#solaireAttackButton").on("click", function(){
+    console.log(chosenCharacter);
+    if ($(chosenCharacter).is("#artorias")){
+        alert("artorias attacked");
+    } else if ($(chosenCharacter).is("#ornstein")){
+        alert("ornstein attacked");
+    } else if ($(chosenCharacter).is("#blackKnight")){
+        alert("black knight attacked");
     }
 })
-//the rest of the characters are unavailable to the player
+//ornstein functions
+$("#ornsteinButton").on("click", function chooseOpponent(){
+    if(!opponentChosen){
+    opponentChosen = true;
+    $("#ornstein").appendTo("#defenderRow");
+    $("button").filter($("#defenderRow button.btn")).toggle();
+    $("button").filter($("#opponentSelectionRow button.choose")).toggle();
+}
+})
 
-//player must choose a character to fight from remaining pool
+$("#ornsteinAttackButton").on("click", function(){
+    console.log(chosenCharacter);
+    if ($(chosenCharacter).is("#artorias")){
+        alert("artorias attacked");
+    } else if ($(chosenCharacter).is("#solaire")){
+        alert("solaire attacked");
+    } else if ($(chosenCharacter).is("#blackKnight")){
+        alert("black knight attacked");
+    }
+})
+//artorias functions
+$("#artoriasButton").on("click", function chooseOpponent(){
+    if(!opponentChosen){
+    opponentChosen = true;
+    $("#artorias").appendTo("#defenderRow");
+    $("button").filter($("#defenderRow button.btn")).toggle();
+    $("button").filter($("#opponentSelectionRow button.choose")).toggle();
+}
+})
 
-//once opponent is chosen move to defender area
+$("#artoriasAttackButton").on("click", function(){
+    console.log(chosenCharacter);
+    if ($(chosenCharacter).is("#solaire")){
+        alert("solaire attacked");
+    } else if ($(chosenCharacter).is("#ornstein")){
+        alert("ornstein attacked");
+    } else if ($(chosenCharacter).is("#blackKnight")){
+        alert("black knight attacked");
+    }
+})
+//black knight functions
+$("#blackKnightButton").on("click", function chooseOpponent(){
+    if(!opponentChosen){
+    opponentChosen = true;
+    $("#blackKnight").appendTo("#defenderRow");
+    $("button").filter($("#defenderRow button.btn")).toggle();
+    $("button").filter($("#opponentSelectionRow button.choose")).toggle();
+}
+})
 
+$("#blackKnightAttackButton").on("click", function(){
+    console.log(chosenCharacter);
+    if ($(chosenCharacter).is("#artorias")){
+        alert("artorias attacked");
+    } else if ($(chosenCharacter).is("#ornstein")){
+        alert("ornstein attacked");
+    } else if ($(chosenCharacter).is("#solaire")){
+        alert("solaire attacked");
+    }
+})
 //The player will now be able to click the attack button.
 
 //Whenever the player clicks attack, their character damages the defender. The opponent will lose HP (health points). These points are displayed at the bottom of the defender's picture. The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their HP. These points are shown at the bottom of the player character's picture.
